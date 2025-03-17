cf66cb76e31256eff9d742e808af2813f3ffc086
Akshat Goyal <goyalakshat111@gmail.com>
2025-03-17 15:39:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname = document.querySelectorAll("input")[0];
+	let lname = document.querySelectorAll("input")[1];
+	alert(`${fname.value} ${lname.value}`);
 }
